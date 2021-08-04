be850d34ddc7551b1dbc9181164f368c7d73137f
Yusuf Keten <ketenyusuf@gmail.com>
2021-08-04 20:53:53 +0300

    Vertex Buffer Layouts

--- Lambda/src/Platform/OpenGL/OpenGLBuffer.h
+++ Lambda/src/Platform/OpenGL/OpenGLBuffer.h
@@ -11,11 +11,15 @@ namespace Lambda
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		void Bind() const override;
+		void Unbind() const override;
+
+		const BufferLayout& GetLayout() const override { return m_Layout; }
+		void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -24,8 +28,8 @@ namespace Lambda
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		void Bind() const override;
+		void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 
