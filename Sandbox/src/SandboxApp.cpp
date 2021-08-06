3e0d5d77ee05759e88905dedf9c2e7d22f912de7
Yusuf Keten <ketenyusuf@gmail.com>
2021-08-07 01:01:58 +0300

    Simple material

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -91,7 +91,7 @@ public:
 
 		m_Shader.reset(new Lambda::Shader(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in  vec3 a_Position;
@@ -108,21 +108,23 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out  vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
 
-		m_BlueShader.reset(new Lambda::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(new Lambda::Shader(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Lambda::Timestep ts) override
@@ -155,6 +157,8 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
 
 		for(int y = 0; y < 20; y++)
 		{
@@ -162,7 +166,12 @@ public:
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Lambda::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				if (x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+
+				Lambda::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -185,7 +194,7 @@ private:
 	std::shared_ptr<Lambda::Shader> m_Shader;
 	std::shared_ptr<Lambda::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Lambda::Shader> m_BlueShader;
+	std::shared_ptr<Lambda::Shader> m_FlatColorShader;
 	std::shared_ptr<Lambda::VertexArray> m_SquareVA;
 
 	Lambda::OrthographicCamera m_Camera;
