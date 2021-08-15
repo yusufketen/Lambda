ccde0b10840f0eb6f5ce4f289430c99d4d0f63e4
Yusuf Keten <ketenyusuf@gmail.com>
2021-08-15 20:22:44 +0300

    Avoid using std::function in event dispatcher for performance

--- Lambda/src/Lambda/Events/Event.h
+++ Lambda/src/Lambda/Events/Event.h
@@ -48,8 +48,6 @@ namespace Lambda {
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 	public:
 		EventDispatcher(Event& event)
 			: m_Event(event)
@@ -57,12 +55,13 @@ namespace Lambda {
 
 		}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		// F will be deduced by the compiler
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
