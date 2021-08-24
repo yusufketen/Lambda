81adfef6aec68b42df802e016f58c74628072b19
Yusuf Keten <ketenyusuf@gmail.com>
2021-08-24 18:25:57 +0300

    Transform Component UI

--- Lambda/src/Lambda/Scene/Scene.cpp
+++ Lambda/src/Lambda/Scene/Scene.cpp
@@ -39,7 +39,7 @@ namespace Lambda
 	{
 
 		const Camera* mainCamera = nullptr;
-		glm::mat4* cameraTransform = nullptr;
+		glm::mat4 cameraTransform;
 		{
 			auto view = m_Registry.view<TransformComponent, CameraComponent>();
 			for (auto entity : view)
@@ -49,7 +49,7 @@ namespace Lambda
 				if(camera.Primary)
 				{
 					mainCamera = &camera.Camera;
-					cameraTransform = &transform.Transform;
+					cameraTransform = transform.GetTransform();
 					break;
 				}
 			}
@@ -57,14 +57,14 @@ namespace Lambda
 
 		if(mainCamera)
 		{
-			Renderer2D::BeginScene(mainCamera->GetProjection(), *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto& [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 
 			Renderer2D::EndScene();
