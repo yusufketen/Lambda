35fa5ee7ad194e612752c2e5648ccc817a00d1af
Yusuf Keten <ketenyusuf@gmail.com>
2021-08-27 22:57:45 +0300

    New Lambda Theme

--- Lambda/src/Lambda/ImGui/ImGuiLayer.h
+++ Lambda/src/Lambda/ImGui/ImGuiLayer.h
@@ -20,7 +20,11 @@ namespace Lambda {
 
 		void Begin();
 		void End();
+
 		void BlockEvents(bool block) { m_BlockEvents = block; }
+
+		void SetDarkThemeColors();
+
 	private:
 		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
